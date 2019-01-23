c861f3256578c11bff3438e92d5396389b80596d
LeeChangYoung <lcy2080@utplus.co.kr>
2019-01-23 11:14:13 +0900

    Version 4.21.1 공부를 위한 프로젝트이므로 Starter Content를 사용할 계획이기 때문에 StarterContent를 포함한다.

---
+++ Source/UnrealStudyAlone/UnrealStudyAloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealStudyAloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTUDYALONE_API AUnrealStudyAloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
